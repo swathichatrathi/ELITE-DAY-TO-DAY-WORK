a7ef0eb3712347f1ed18fed3ef551f6a62b009b6
Swathi Chatrathi <85331659+swathichatrathi@users.noreply.github.com>
2023-01-31 23:51:34 +0530

    Create LONGEST SUBARRAY 0 SUM.java

---
+++ 31-01-23/LONGEST SUBARRAY 0 SUM.java
@@ -0,0 +1,33 @@
+// LONGEST SUBARRAY 0 SUM
+
+//SOLUTION IN JAVA 
+
+import java.util.* ;
+import java.io.*; 
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+
+	public static int LongestSubsetWithZeroSum(ArrayList<Integer> arr) {
+		HashMap<Integer,Integer>map=new HashMap<>();
+        int maxl=0;
+        int sum=0; 
+        for(int i=0;i<arr.size();i++){
+            sum+=arr.get(i); 
+            if(sum==0){
+                maxl=i+1; 
+            }
+            else{
+                if(map.containsKey(sum)){
+                    maxl=Math.max(maxl,i-map.get(sum)); 
+                }
+                else{
+                    map.put(sum,i); 
+                }
+            }
+        }
+        return maxl; 
+    }
+}
