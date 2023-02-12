c98e9263b1d618952cb59487c2c36780e095ab94
Swathi Chatrathi <85331659+swathichatrathi@users.noreply.github.com>
2023-02-12 11:17:13 +0530

    Create FIND CENTER OF STAR GRAPH.java

---
+++ 12-02-23/FIND CENTER OF STAR GRAPH.java
@@ -0,0 +1,14 @@
+// GRAPHS 
+
+// FIND CENTER OF STAR GRAPH
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        if((edges[0][0]==edges[1][0]) || (edges[0][0]==edges[1][1])){
+            return edges[0][0];
+        }
+        else{
+            return edges[0][1];
+        }
+    }
+}
